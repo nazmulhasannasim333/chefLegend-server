189ea4a77f05c5760ab14bf4488563cbd76a361a
Md Nasim Hosen <mdnasimhosen333@gmail.com>
2023-05-03 02:38:22 +0600

    server don't load categorires, change some code and run again

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/chefCategories", (req, res) => {
-  res.send(chefCategorie);
+  res.send(chefCategories);
 });
 app.get("/chefCategories/:id", (req, res) => {
   const id = req.params.id;
